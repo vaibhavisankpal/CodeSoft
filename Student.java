0f0c195efad3e5b4a603e6ac3d73d9f143075dbf
vaibhavisankpal <130655018+vaibhavisankpal@users.noreply.github.com>
2023-09-18 22:10:54 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,28 @@
+package com;
+
+
+public class Student {
+    private String name;
+    private String rollNumber;
+    private String grade;
+
+    public Student(String name, String rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    
+}
